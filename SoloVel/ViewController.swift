83ea74013d60bc77d7c491b0687f59b6ddf3f519
hirapark8823 <90318741+hirapark8823@users.noreply.github.com>
2021-12-03 18:06:04 +0900

    prototype_ver2

---
+++ SoloVel/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  SoloVel
+//
+//  Created by 平本尚寛 on 2021/12/03.
+//
+
+import UIKit
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var registerEmailTextField: UITextField!
+    @IBOutlet weak var registerPasswordTextField: UITextField!
+    @IBOutlet weak var registerNameTextField: UITextField!
+    @IBOutlet weak var loginEmailTextField: UITextField!
+    @IBOutlet weak var loginPasswordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func tapRegisterButton(_ sender: Any) {
+       
+    }
+    
+    @IBAction func tapLoginButton(_ sender: Any) {
+        
+    }
+}
+
+
